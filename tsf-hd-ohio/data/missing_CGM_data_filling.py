d0011c129a1f557f8a6254fe6934769345ea5d18
Congge Xu <conggexu@Congges-MacBook-Pro.local>
2024-05-15 20:34:05 -0700

    update filling_CGM

--- tsf-hd-ohio/data/missing_CGM_data_filling.py
+++ tsf-hd-ohio/data/missing_CGM_data_filling.py
@@ -17,18 +17,55 @@ def remove_nan_strat_end(df, attr):
                 break
         df = df[index_start:index_end]
         df = df.reset_index(drop=True)
-        print("Preprocessing: Remove nan successfully!")
         return df
     else:
         print('Unable to remove: No Attibutes Found\n')
         return 0
 
 
-# fill in nan values with "1"
-# TODO: use average value to fill in missing CGMs 
-def filling_CGM(testpath):
-    filepath = "../dataset/processedcsv/ohio540.csv"
-    a_test = pd.read_csv(filepath, usecols=['CGM'])
-    a_test = remove_nan_strat_end(a_test, 'CGM')  
-    AA = a_test['CGM'].fillna(1)
-    return AA
+# fill in nan values with 
+def filling_CGM(df_data) -> pd.Series:  
+    """
+    Fill NaN values in the 'CGM' column of the DataFrame with the average of nearby non-null values.
+
+    Parameters:
+        df_data (pd.DataFrame): Input DataFrame containing 'Time' and 'CGM' columns.
+
+    Returns:
+        pd.Series: Series containing the cleaned 'CGM' column data.
+    """
+    df_copy = df_data.copy() # avoid modifying original value
+    df_copy = remove_nan_strat_end(df_copy, 'CGM')  
+    df_copy['Time'] = pd.to_datetime(df_copy['Time'], format='%d-%b-%Y %H:%M:%S')
+    
+    for index, row in df_copy.iterrows():
+        if pd.isna(row['CGM']):
+            # Extract hour and minute from the current time
+            hour_minute = row['Time'].strftime('%H:%M')
+
+            # Find rows with non-null CGM values matching the same hour and minute
+            matched_rows = df_copy[(~df_copy['CGM'].isna()) & (df_copy['Time'].dt.strftime('%H:%M') == hour_minute)]
+
+            if len(matched_rows) >= 3:
+                # Select the current row and the next two consecutive rows
+                selected_rows = matched_rows.iloc[:3]
+
+                # Calculate the average of the CGM values
+                avg_cgm_value = selected_rows['CGM'].mean()
+
+                # Replace the NaN CGM value with the calculated average
+                df_copy.at[index, 'CGM'] = avg_cgm_value
+ 
+    if df_copy['CGM'].isna().sum() == 0:
+        print("Remove NaN successfully")
+    else:
+        print("Remove NaN failed")
+        return 
+    
+    return df_copy['CGM']
+    
+    
+    
+    
+    # AA = df_copy['CGM'].fillna(150)
+    # return AA
